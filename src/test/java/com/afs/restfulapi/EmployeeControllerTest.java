9ecbcb84a98932fa9dd52040fb2d14960702afc9
BennyYuen09 <Benny.Yuen@agileexlab.net>
2021-10-26 22:07:49 +0800

    [RestfulAPI][Benny] test: add test case for get a employee list page from employee controller given page and page size

--- src/test/java/com/afs/restfulapi/EmployeeControllerTest.java
+++ src/test/java/com/afs/restfulapi/EmployeeControllerTest.java
@@ -2,6 +2,7 @@ package com.afs.restfulapi;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,12 +21,26 @@ public class EmployeeControllerTest {
     }
 
     @Test
-    void should_return_correct_employee_when_get_employee_list_given_employee_id() {
+    void should_return_correct_employee_when_get_employee_given_employee_id() {
         //given
         Employee expected = new EmployeeRepository().getEmployeeById(1);
 
         //when
         Employee actual = new EmployeeController().getEmployeeById(1);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_return_employee_list_page_when_get_employee_list_page_given_page_and_page_size() {
+        //given
+        List<Employee> employeeList = new EmployeeRepository().getEmployeeList();
+        List<Employee> expected = new ArrayList<>();
+        expected.add(employeeList.get(1));
+
+        //when
+        List<Employee> actual = new EmployeeController().getEmployeeListByPage(1, 1);
 
         //then
         assertEquals(expected, actual);
