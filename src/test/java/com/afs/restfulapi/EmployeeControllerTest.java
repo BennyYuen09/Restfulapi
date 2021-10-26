1fba0794be052d29c50d2163340f73ce743fce5c
BennyYuen09 <Benny.Yuen@agileexlab.net>
2021-10-26 22:04:43 +0800

    [RestfulAPI][Benny] test: add test case for get a employee from employee controller using employee id

--- src/test/java/com/afs/restfulapi/EmployeeControllerTest.java
+++ src/test/java/com/afs/restfulapi/EmployeeControllerTest.java
@@ -8,12 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EmployeeControllerTest {
     @Test
-    void should_return_employee_list_when_get_employee_list_given_a_employee_repository() {
+    void should_return_employee_list_when_get_employee_list_given_employee_repository() {
         //given
         List<Employee> expected = new EmployeeRepository().getEmployeeList();
 
         //when
         List<Employee> actual = new EmployeeController().getEmployeeList();
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_return_correct_employee_when_get_employee_list_given_employee_id() {
+        //given
+        Employee expected = new EmployeeRepository().getEmployeeById(1);
+
+        //when
+        Employee actual = new EmployeeController().getEmployeeById(1);
 
         //then
         assertEquals(expected, actual);
