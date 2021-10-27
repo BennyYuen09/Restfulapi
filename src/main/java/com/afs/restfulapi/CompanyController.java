e10ae20e39ffd0566c81183efdd062579dc30d15
BennyYuen09 <Benny.Yuen@agileexlab.net>
2021-10-27 08:36:00 +0800

    [RestfulAPI][Benny] feat: all controller will have real database storage now rather than instantiate everytime the method is called

--- src/main/java/com/afs/restfulapi/CompanyController.java
+++ src/main/java/com/afs/restfulapi/CompanyController.java
@@ -9,14 +9,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
+    private final CompanyRepository companyRepository;
+
+    public CompanyController(){
+        companyRepository = new CompanyRepository();
+    }
+
     @GetMapping
     public List<Company> getCompanyList() {
-        return new CompanyRepository().getCompanyList();
+        return this.companyRepository.getCompanyList();
     }
 
     @GetMapping("/{id}")
     public Company getCompanyById(@PathVariable("id") Integer id) {
-        return new CompanyRepository().getCompanyById(id);
+        return this.companyRepository.getCompanyById(id);
     }
 
     @GetMapping("/{id}/employees")
@@ -27,22 +33,22 @@ public class CompanyController {
     @RequestMapping(params = {"page", "pageSize"}, method = RequestMethod.GET)
     public List<Company> getCompanyListByPage(@RequestParam(value = "page") int page,
                                               @RequestParam(value = "pageSize") int pageSize) {
-        return new CompanyRepository().getCompanyByPage(page, pageSize);
+        return this.companyRepository.getCompanyByPage(page, pageSize);
     }
 
     @PostMapping
     public Company addCompany(@RequestBody Company company) {
-        return new CompanyRepository().addCompany(company);
+        return this.companyRepository.addCompany(company);
     }
 
     @PutMapping("/{id}")
     public Company updateCompanyById(@PathVariable("id") Integer id, @RequestBody Company company) {
-        return new CompanyRepository().updateCompany(id, company);
+        return this.companyRepository.updateCompany(id, company);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteCompanyById(@PathVariable("id") Integer id) {
-        boolean isRemoved = new CompanyRepository().deleteEmployeeById(id);
+        boolean isRemoved = this.companyRepository.deleteEmployeeById(id);
         if (isRemoved) {
             return new ResponseEntity<>("Delete Company ID: " + id, HttpStatus.OK);
         }
--- src/main/java/com/afs/restfulapi/EmployeeController.java
+++ src/main/java/com/afs/restfulapi/EmployeeController.java
@@ -9,40 +9,46 @@ import java.util.List;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(){
+        employeeRepository = new EmployeeRepository();
+    }
+
     @GetMapping
     public List<Employee> getEmployeeList() {
-        return new EmployeeRepository().getEmployeeList();
+        return this.employeeRepository.getEmployeeList();
     }
 
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable("id") Integer id) {
-        return new EmployeeRepository().getEmployeeById(id);
+        return this.employeeRepository.getEmployeeById(id);
     }
 
     @RequestMapping(params = {"page", "pageSize"}, method = RequestMethod.GET)
     public List<Employee> getEmployeeListByPage(@RequestParam(value = "page") int page,
                                                 @RequestParam(value = "pageSize") int pageSize) {
-        return new EmployeeRepository().getEmployeeListByPage(page, pageSize);
+        return this.employeeRepository.getEmployeeListByPage(page, pageSize);
     }
 
     @RequestMapping(params = {"gender"}, method = RequestMethod.GET)
     public List<Employee> getEmployeeListByGender(@RequestParam(value = "gender") String gender) {
-        return new EmployeeRepository().getEmployeeListByGender(gender);
+        return this.employeeRepository.getEmployeeListByGender(gender);
     }
 
     @PostMapping
     public Employee addEmployee(@RequestBody Employee employee) {
-        return new EmployeeRepository().addEmployee(employee);
+        return this.employeeRepository.addEmployee(employee);
     }
 
     @PutMapping("/{id}")
     public Employee updateEmployeeById(@PathVariable("id") Integer id, @RequestBody Employee employee) {
-        return new EmployeeRepository().updateEmployee(id, employee);
+        return this.employeeRepository.updateEmployee(id, employee);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteEmployeeById(@PathVariable("id") Integer id) {
-        boolean isRemoved = new EmployeeRepository().deleteEmployeeById(id);
+        boolean isRemoved = this.employeeRepository.deleteEmployeeById(id);
         if (isRemoved) {
             return new ResponseEntity<>("Delete Employee ID: " + id, HttpStatus.OK);
         }
