8723955e05f1d16ae3b951bcc27b95fda7daae22
BennyYuen09 <Benny.Yuen@agileexlab.net>
2021-10-29 20:37:55 +0800

    [RestfulAPI][Benny] test: add findall test case of company service

---
+++ src/test/java/com/afs/restfulapi/CompanyServiceTest.java
@@ -0,0 +1,45 @@
+package com.afs.restfulapi;
+
+import com.afs.restfulapi.company.Company;
+import com.afs.restfulapi.company.CompanyRepository;
+import com.afs.restfulapi.company.CompanyService;
+import com.afs.restfulapi.employee.Employee;
+import com.afs.restfulapi.employee.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class CompanyServiceTest {
+    @Mock
+    private CompanyRepository companyRepository;
+    @Mock
+    private EmployeeRepository employeeRepository;
+    @InjectMocks
+    private CompanyService companyService;
+
+    @Test
+    void should_return_all_company_when_get_company_list_given_3_company() {
+        //given
+        List<Company> expected = Arrays.asList(
+                new Company("Axel"),
+                new Company("UB_soft"),
+                new Company("EA_trash")
+        );
+        when(companyRepository.findAll()).thenReturn(expected);
+
+        //when
+        List<Company> actual = companyService.findAll();
+
+        //then
+        assertEquals(expected, actual);
+    }
+}
